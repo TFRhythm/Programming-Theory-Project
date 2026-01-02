49b117441b539d3adb6e2aae0ed63eefb685a853
TFRhythm <108201455+TFRhythm@users.noreply.github.com>
2026-01-02 00:02:59 -0800

    Project Notes
    Added notes for the locations of the 4 Pillars of OOP (Abstraction, Inheritance, Polymorphism, Encapsulation) per the project guidelines for the Unity Jr Programmer Pathway

--- Assets/VerticalHazard.cs
+++ Assets/VerticalHazard.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//INHERITANCE
 public class VerticalHazard : BasicHazard
 {
+
+    //POLYMORPHISM
     protected override Vector2 RecoilDirectionCalculator()
     {
         if (this.transform.position.x < PlayerRb2D.transform.position.x)
